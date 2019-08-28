1fce83c710c7ae74d6c00be17d4bd5ff085aba84
Aliaksei Balabushka <aliaksei.balabushka@gmail.com>
2019-08-28 21:07:57 +0200

    Authorised routes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Route } from 'react-router-dom';
 import HomePage from './components/pages/home-page';
 import LoginPage from './components/pages/login-page';
+import DashboardPage from './components/pages/dashboard-page';
+import UserRoute from './components/routes/user-route';
+import GuestRoute from './components/routes/guest-route';
 
 import 'semantic-ui-css/semantic.min.css';
 
-const App = () => {
+const App = ({ location }) => {
   return (
   <div className="ui container">
-    <Route path="/" exact component={ HomePage } />
-    <Route path="/login" exact component={ LoginPage } />
+    <Route location={location} path="/" exact component={ HomePage } />
+    <GuestRoute location={location} path="/login" exact component={ LoginPage } />
+    <UserRoute location={location} path="/dashboard" exact component={ DashboardPage } />
   </div>
   );
+};
+
+App.propTypes = {
+  location: PropTypes.shape({
+    pathname: PropTypes.string.isRequired
+  }).isRequired
 };
 
 export default App;
